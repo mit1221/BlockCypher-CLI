fbc7768688f5d56dacecc6d8640025b1db5506a0
Mit <mit_rupang@yahoo.ca>
2018-11-07 16:38:53 -0500

    Updated README

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Use Blockcypher's API to return the balance of a specific bitcoin testnet address and make a payment using testnet bitcoins
 
 URL to clone this repo: https://github.com/mit1221/Coding-Challenge.git
+
+1. Run `npm install` to install all dependencies.
+2. Run `node app` to run the program.
--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+// import required modules
 const request = require('request');
+var prompt = require('prompt');
 
-request('https://api.blockcypher.com/v1/btc/test3/addrs/n4VQ5YdHf7hLQ2gWQYYrcxoE5B7nWuDFNF/balance', {
-  json: true
-}, (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log("The current balance in the testnet address is: " + body.balance);
+// get testnet address from the user and make a request to blockcypher's API
+prompt.start();
+prompt.get(['address'], function (err, result) {
+  request('https://api.blockcypher.com/v1/btc/test3/addrs/' + result.address + '/balance', {
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The current balance in the testnet address " + result.address + " is: " + body.balance);
+  });
 });
